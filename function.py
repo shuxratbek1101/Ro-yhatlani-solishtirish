a8e8eb3a173693281bbde6f7000c5ad237fc75c0
shuxratbek1101 <131897342+shuxratbek1101@users.noreply.github.com>
2024-02-23 10:24:07 +0500

    Add files via upload

---
+++ function.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 23 08:56:00 2024
+
+@author: user
+"""
+#1
+#def about_users(ismi,familiya,tyili,joriy_yil=2024):
+#    """foydalanuvchi haqida malumot beradigan funksiya"""
+#    print(f"Foydalanuvchi ismi-{ismi.title()}"
+#          f"\nFoydalanuvchi familiyasi-{familiya.title()}"
+#          f"\nFoydalanuvchi yoshi-{joriy_yil-int(tyili)}"
+#          )
+#about_users('shuxratbek', 'tojimuratov', 2003)
+#-----------------------------------------------------------------------------
+#2
+#def kvandkub(son):
+#    """sonni kvadrati va kubini hisoblaydigan dastur"""
+#    print(f"sonning kvadrati-{int(son)**2}"
+#          f"\nsonning kubi-{int(son)**3}")
+#kvandkub(5)
+#3
+#def odd_and_even(son)   :
+#    """sonni toq yoki juftligini aniqlovchi funksiya"""
+#    if son%2==0:
+#        print(f"{son}->juft son")
+#    else:
+#        print(f"{son}->toq son")
+    
+#odd_and_even(5)
+#------------------------------------------------------------------------------
+#4
+# def compare(a,b):
+#     """sonlarni taqqoslovchi funksiya"""
+   
+#     if a>b :
+#         print(f"{a}-katta son")
+#     elif b>a:
+#         print(f"{b} katta son")
+#     else:
+       
+#         print('ikki son teng')
+        
+# compare(5, 5)
+#==============================================================================
+#5
+# def daraja(x,y):
+#     """x ni y-sini hisoblovchi dastur"""
+#     print(f"{x**y}")
+    
+# daraja(2, 5)
+#=============================================================================
+#6
+# def daraja(x,y=2):
+#     """x ni y-sini hisoblovchi dastur"""
+#     print(f"{x**y}")
+    
+# daraja(2)
+#==============================================================================
+#7
+# def qoldiq(a):
+#     """a sonni 2dan 10gacha sonlarga qoldiqsiz bo'linishini hisoblovchi"""
+
+#     for n in range(2,11):
+#         if a%n==0:
+#             print(f"{a}, {n}ga  soni qoldiqsiz bo'linadi")
+
+# qoldiq(70)
+
